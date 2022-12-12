0069f7e5d15515d957fb14c81a51a0b644f2681b
Ivan Safonov <safonovivv@gmail.com>
2022-12-12 22:45:57 +0400

    Text box tests complete *Присутствует проблема с масштабированием окна браузера!

--- src/test/java/textBox_Tests.java
+++ src/test/java/textBox_Tests.java
@@ -2,10 +2,11 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-//import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver;
 
-//import java.awt.*;
+import java.awt.*;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -19,13 +20,22 @@ public class textBox_Tests {
     }
     @Test
     void FillFormTest() {
+        String userName = "Ivan Safonov";
+        String userEmail = "safonovivv@gmail.com";
+        String currentAddress = "city of Moscow 1";
+        String permanentAddress = "city of Moscow 2";
+
         open("/text-box");
-        $("#userName").setValue("First_lastName");
-        $("#userEmail").setValue("First_lastName@Email.com");
-        $("#currentAddress").setValue("Current address");
-        $("#permanentAddress").setValue("Permanent address");
+        $("#userName").setValue(userName);
+        $("#userEmail").setValue(userEmail);
+        $("#currentAddress").setValue(currentAddress);
+        $("#permanentAddress").setValue(permanentAddress);
         $("#submit").click();
 
         $("#output").shouldBe(Condition.visible);
+        $("#output #name").shouldHave(text(userName));
+        $("#output #email").shouldHave(text(userEmail));
+        $("#output #currentAddress").shouldHave(text(currentAddress));
+        $("#output #permanentAddress").shouldHave(text(permanentAddress));
     }
 }
